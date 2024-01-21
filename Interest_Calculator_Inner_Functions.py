e1ab76540828d92b4814a52488d3e26619ba9d9c
Hitanshu Janakbhai Darji <130683633+HitanshuDarji@users.noreply.github.com>
2024-01-20 23:32:26 -0700

    Fixed some minor bugs.

--- Interest_Calculator_Inner_Functions.py
+++ Interest_Calculator_Inner_Functions.py
@@ -2,25 +2,28 @@ def main():
     principal = input("Please enter the principal amount: ")
     interest_rate = input("Please enter the rate of interest: ")
     years = input("Please enter the number of years: ")
-    print(interest_calculator(principal, interest_rate, years))
+    print(
+        f"Total interest amount: {interest_calculator(principal, interest_rate, years)}")
 
 
 def interest_calculator(p, r, n):
     # Validating the user input against the required parameters using if statements.
-    if False in [p.isdigit(), r.isdigit(), n.isdigit()]:
+    try:
+        if float(r) > 100:
+            raise ValueError(
+                "ERROR: Rate of interest should be less than 100.")
+        if float(n) > 12:
+            raise ValueError("ERROR: Number of years should be less than 12.")
+        if float(p) < 0 or float(r) < 0 or float(n) < 0:
+            raise ValueError(
+                "ERROR: Principal, Interest and Years cannot be negative values.")
+    except ValueError:
         raise ValueError(
-            "Invalid input! Please make sure all inputs are numbers.")
-    if float(r) > 100:
-        raise ValueError("ERROR: Rate of interest should be less than 100.")
-    if float(n) > 12:
-        ValueError("ERROR: Number of years should be less than 12.")
-    if float(p) < 0 or float(r) < 0 or float(n) < 0:
-        raise ValueError(
-            "ERROR: Principal, Interest and Years cannot be negative values.")
+            "ERROR: Invalid Input! Please make sure all the inputs are numbers.")
 
     # Calculating the interest.
     def actual_calculator():
-        return float(p) * float(r) * float(n)
+        return float(p) * (float(r) / 100) * float(n)
 
     # Calculating and returning the rate of interest.
     return actual_calculator()
